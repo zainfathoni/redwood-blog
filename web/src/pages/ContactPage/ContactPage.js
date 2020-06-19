11cd2d2e00afe7a0d9855966d1dc6ad1ff2e3278
Zain Fathoni <zain.fathoni@gmail.com>
2020-06-19 10:42:00 +0800

    Build a Form in the ContactPage

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,10 +1,47 @@
 import BlogLayout from 'src/layouts/BlogLayout'
+import {
+  Form,
+  Label,
+  TextField,
+  TextAreaField,
+  FieldError,
+  Submit,
+} from '@redwoodjs/web'
 
 const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
     <BlogLayout>
-      <h1>ContactPage</h1>
-      <p>Find me in ./web/src/pages/ContactPage/ContactPage.js</p>
+      <Form onSubmit={onSubmit} validation={{ mode: 'onBlur' }}>
+        <Label errorClassName="error" name="name" />
+        <TextField
+          name="name"
+          errorClassName="error"
+          validation={{ required: true }}
+        />
+        <FieldError className="error" name="name" />
+
+        <Label errorClassName="error" name="email" />
+        <TextField
+          name="email"
+          errorClassName="error"
+          validation={{ required: true, pattern: { value: /[^@]+@[^.]+\..+/ } }}
+        />
+        <FieldError className="error" name="email" />
+
+        <Label errorClassName="error" name="message" />
+        <TextAreaField
+          name="message"
+          errorClassName="error"
+          validation={{ required: true }}
+        />
+        <FieldError className="error" name="message" />
+
+        <Submit>Save</Submit>
+      </Form>
     </BlogLayout>
   )
 }
