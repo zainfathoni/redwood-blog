6c5f96470227b9523ae2658abd69886b7d9354e3
Zain Fathoni <zain.fathoni@gmail.com>
2020-06-19 10:44:45 +0800

    yarn rw g sdl contact

---
+++ api/src/graphql/contacts.sdl.js
@@ -0,0 +1,27 @@
+import gql from 'graphql-tag'
+
+export const schema = gql`
+  type Contact {
+    id: Int!
+    name: String!
+    email: String!
+    message: String!
+    createdAt: DateTime!
+  }
+
+  type Query {
+    contacts: [Contact!]!
+  }
+
+  input CreateContactInput {
+    name: String!
+    email: String!
+    message: String!
+  }
+
+  input UpdateContactInput {
+    name: String
+    email: String
+    message: String
+  }
+`
